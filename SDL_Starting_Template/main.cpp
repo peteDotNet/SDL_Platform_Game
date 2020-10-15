a5cd9d945eef9d3fc9e5a7bc63a92853659fa479
Peter Morton <petermorton@live.co.uk>
2020-10-15 14:53:27 +0100

    Tutorial 3: Limiting frame rate

--- SDL_Starting_Template/main.cpp
+++ SDL_Starting_Template/main.cpp
@@ -5,14 +5,30 @@ Game* game = nullptr;
 
 int main(int argc, char* argv[])
 {
+	const int fps = 60;
+	const int frameDelay = 1000 / fps;
+
+	Uint32 frameStart;
+	int frameTime;
+
 	game = new Game();
 	game->init("Platform Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
 
 	while (game->running())
 	{
+		frameStart = SDL_GetTicks();
+
+
 		game->handleEvents();
 		game->update();
 		game->render();
+
+		frameTime = SDL_GetTicks() - frameStart;
+
+		if (frameTime < frameDelay)
+		{
+			SDL_Delay(frameDelay - frameTime);
+		}
 
 	}
 
